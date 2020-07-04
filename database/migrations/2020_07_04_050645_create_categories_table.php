82432632a126735d94ce0a59d34ac0999eb62d19
Sergey <sssssergo15@gmail.com>
2020-07-04 16:03:06 +0600

    add migrations

--- database/migrations/2020_07_04_050645_create_categories_table.php
+++ database/migrations/2020_07_04_050645_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->integer('is_admin')->default(0);
-            $table->integer('is_bannded')->default(0);
-            $table->rememberToken();
+            $table->string('slug');
+            $table->string('title');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 }
